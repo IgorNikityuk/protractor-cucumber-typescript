7dd2de84989f7bc26db29686eb0c186142bf9061
IgorN <igor.nikityuk@gmail.com>
2016-11-07 06:52:18 -0500

    updates

--- StepDefinitions/resultsPage.ts
+++ StepDefinitions/resultsPage.ts
@@ -1,4 +1,5 @@
 import { ResultsPage } from '../Pages/resultsPage';
+import { browser } from 'protractor';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
@@ -6,7 +7,9 @@ export = function () {
     let results = new ResultsPage();
 
     this.Then(/^Sign in button should be displayed$/, () => {
+        browser.sleep(2000)
         return expect(results.firstResultLink.isDisplayed()).to.eventually.equal(true);
+
     });
 
 }
